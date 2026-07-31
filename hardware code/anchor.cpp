ff608070403eaf49d3a90cd41c6c20c0a8914225
Betsalely <yairbetsalel2.0@gmail.com>
2026-07-31 14:23:45 +1200

    Anchor v2

--- hardware code/anchor.cpp
+++ hardware code/anchor.cpp
@@ -1,66 +1,85 @@
 #include <SPI.h>
 #include <DW3000.h>
 
-int p1 = 9;
-int p2 = 10;
-int p3 = 11;
-int p4 = 12;
-int p5 = 13;
-int p6 = 14;
+#define PIN_RST 9
+#define PIN_SS 10
+#define PIN_MOSI 11
+#define PIN_SCK 12
+#define PIN_MISO 13
+#define PIN_IRQ 14
+
+#define MAX_BEACONS 8
+#define SPEED_OF_LIGHT 299792458.0
 
 struct BeaconData
 {
-    int id;
+    uint8_t id;
     float x;
     float y;
     float z;
-    float d;
+    float distance;
+    bool updated;
 };
 
-BeaconData b[8];
-int b_cnt = 0;
+BeaconData beacons[MAX_BEACONS];
+uint8_t beacon_count = 0;
 
-struct PollPacket
+struct __attribute__((packed)) PollPacket
 {
-    uint8_t type;
-    uint8_t tid;
+    uint8_t msg_type;
+    uint8_t target_id;
 };
 
-struct BeaconPacket
+struct __attribute__((packed)) BeaconPacket
 {
-    uint8_t type;
-    uint8_t bid;
+    uint8_t msg_type;
+    uint8_t beacon_id;
     float x;
     float y;
     float z;
-    uint32_t rt;
-    uint32_t tt;
+    uint32_t rx_timestamp;
+    uint32_t tx_timestamp;
 };
 
-bool calc(float &ox, float &oy)
+bool calculateAnchorPosition(float &out_x, float &out_y)
 {
-    if (b_cnt < 3)
-        return false;
-
-    float A[8][2];
-    float B[8];
-
-    int n = b_cnt - 1;
-    float xn = b[n].x;
-    float yn = b[n].y;
-    float rn = b[n].d;
-
-    for (int i = 0; i < n; i++)
+    int valid_beacons = 0;
+    for (int i = 0; i < beacon_count; i++)
     {
-        A[i][0] = xn - b[i].x;
-        A[i][1] = yn - b[i].y;
-        B[i] = (b[i].d * b[i].d - rn * rn) - (b[i].x * b[i].x - xn * xn) - (b[i].y * b[i].y - yn * yn);
+        if (beacons[i].updated)
+            valid_beacons++;
+    }
+
+    if (valid_beacons < 3)
+    {
+        return false;
+    }
+
+    float A[MAX_BEACONS][2];
+    float B[MAX_BEACONS];
+
+    int ref_idx = valid_beacons - 1;
+    float xN = beacons[ref_idx].x;
+    float yN = beacons[ref_idx].y;
+    float rN = beacons[ref_idx].distance;
+
+    int row = 0;
+    for (int i = 0; i < valid_beacons - 1; i++)
+    {
+        float xi = beacons[i].x;
+        float yi = beacons[i].y;
+        float ri = beacons[i].distance;
+
+        A[row][0] = 2 * (xN - xi);
+        A[row][1] = 2 * (yN - yi);
+        B[row] = (ri * ri - rN * rN) - (xi * xi - xN * xN) - (yi * yi - yN * yN);
+        row++;
     }
 
     float AtA[2][2] = {0};
     float AtB[2] = {0};
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < row; i++)
     {
         AtA[0][0] += A[i][0] * A[i][0];
         AtA[0][1] += A[i][0] * A[i][1];
@@ -72,11 +91,11 @@ bool calc(float &ox, float &oy)
     }
 
     float det = AtA[0][0] * AtA[1][1] - AtA[0][1] * AtA[1][0];
-    if (det == 0)
+    if (abs(det) < 0.0001)
         return false;
 
-    ox = (AtA[1][1] * AtB[0] - AtA[0][1] * AtB[1]) / det;
-    oy = (AtA[0][0] * AtB[1] - AtA[1][0] * AtB[0]) / det;
+    out_x = (AtA[1][1] * AtB[0] - AtA[0][1] * AtB[1]) / det;
+    out_y = (AtA[0][0] * AtB[1] - AtA[1][0] * AtB[0]) / det;
 
     return true;
 }
@@ -84,54 +103,93 @@ bool calc(float &ox, float &oy)
 void setup()
 {
     Serial.begin(115200);
-    SPI.begin();
-    DW3000::select(p2);
-    DW3000::begin(p6, p1);
+    delay(1000);
+    Serial.println("Starting Mobile Anchor Beacon...");
+
+    SPI.begin(PIN_SCK, PIN_MISO, PIN_MOSI, PIN_SS);
+
+    DW3000::select(PIN_SS);
+    if (!DW3000::begin(PIN_IRQ, PIN_RST))
+    {
+        Serial.println("DWM3000 initialization failed!");
+        while (1)
+            ;
+    }
+
     DW3000::setup();
     DW3000::setAntennaDelay(16384);
 }
 
 void loop()
 {
-    for (int id = 1; id <= 4; id++)
+    for (uint8_t id = 1; id <= 4; id++)
     {
-        PollPacket p;
-        p.type = 0x01;
-        p.tid = id;
+        PollPacket poll;
+        poll.msg_type = 0x01;
+        poll.target_id = id;
 
-        int tx = DW3000::getSystemTime();
-        DW3000::sendPacket((uint8_t *)&p, sizeof(p));
+        uint64_t poll_tx_ts = DW3000::getSystemTime();
+        DW3000::sendPacket((uint8_t *)&poll, sizeof(poll));
 
-        while (!DW3000::receivePacket())
+        unsigned long start_time = millis();
+        while (millis() - start_time < 20)
         {
+            if (DW3000::receivePacket())
+            {
+                uint8_t rx_buffer[64];
+                uint16_t rx_len = DW3000::getPacketLength();
+                DW3000::getPacket(rx_buffer, rx_len);
+
+                if (rx_len == sizeof(BeaconPacket))
+                {
+                    BeaconPacket *resp = (BeaconPacket *)rx_buffer;
+
+                    if (resp->msg_type == 0x02 && resp->beacon_id == id)
+                    {
+                        uint64_t resp_rx_ts = DW3000::getRxTimestamp();
+
+                        double tof = (double)(resp_rx_ts - poll_tx_ts) * DW3000_TIME_UNITS;
+                        float distance = (tof * SPEED_OF_LIGHT) / 2.0;
+
+                        bool found = false;
+                        for (int i = 0; i < beacon_count; i++)
+                        {
+                            if (beacons[i].id == id)
+                            {
+                                beacons[i].x = resp->x;
+                                beacons[i].y = resp->y;
+                                beacons[i].z = resp->z;
+                                beacons[i].distance = distance;
+                                beacons[i].updated = true;
+                                found = true;
+                                break;
+                            }
+                        }
+
+                        if (!found && beacon_count < MAX_BEACONS)
+                        {
+                            beacons[beacon_count] = {id, resp->x, resp->y, resp->z, distance, true};
+                            beacon_count++;
+                        }
+
+                        Serial.printf("Beacon %d: Dist = %.2fm | Position = (%.2f, %.2f)\n", id, distance, resp->x, resp->y);
+                        break;
+                    }
+                }
+            }
         }
-
-        uint8_t rx[64];
-        uint16_t rxl = DW3000::getPacketLength();
-        DW3000::getPacket(rx, rxl);
-
-        BeaconPacket *res = (BeaconPacket *)rx;
-
-        if (res->type == 0x02)
-        {
-            int rt = DW3000::getRxTimestamp();
-            float tof = (rt - tx) * 15.65e-12;
-            float dist = (tof * 299792458.0) / 2.0;
-
-            b[b_cnt].id = id;
-            b[b_cnt].x = res->x;
-            b[b_cnt].y = res->y;
-            b[b_cnt].z = res->z;
-            b[b_cnt].d = dist;
-            b_cnt++;
-        }
+        delay(10);
     }
 
-    float ax = 0, ay = 0;
-    if (calc(ax, ay))
+    float anchor_x = 0, anchor_y = 0;
+    if (calculateAnchorPosition(anchor_x, anchor_y))
     {
-        Serial.println(ax);
-        Serial.println(ay);
+        Serial.printf("--> CALCULATED ANCHOR LOCATION: X = %.2fm, Y = %.2fm <--\n", anchor_x, anchor_y);
     }
+    else
+    {
+        Serial.println("Insufficient beacon data for position lock.");
+    }
+
     delay(100);
 }
